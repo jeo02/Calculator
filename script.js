8ae915e4dd7759f024b37a6edaddc726fa847469
jeo02 <juan.ospina@stonybrook.edu>
2021-05-27 20:23:04 -0400

    design + some functionality
    calculator responds to buttons clicked except =

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-function grid(){
-    for(let i = 1; i <= 4; i++){
-        for(let j = 1; j <= 4; j++){
-            let content = document.createElement('button');
-            content.setAttribute("class", "button");
-            document.querySelector("#grid").appendChild(content);
-            content.style.gridColumnStart = j;
-            content.style.gridRowStart = i;
-        }
-    }
+let current = "";
+let formula = "";
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.querySelectorAll('.button');
+    buttons.forEach(button =>{
+        button.addEventListener("click", function(e){
+            current += e.target.textContent;
+            display();
+        })
+    })
+
+    let deleteButton = document.getElementById("delete");
+    deleteButton.addEventListener('click', function(){
+        if(current.length != 0)
+            current = current.substring(0, current.length-1);
+        display();
+    })
+
+    let clearButton = document.getElementById("clear");
+    clearButton.addEventListener('click', function(){
+        current = "";
+        display();
+    })
+
+});
+
+
+function display(){
+    document.querySelector("#formula").innerText = formula;
+    document.querySelector("#currentNum").innerText = current;
+}
+function xd(e){
+    alert(e.target.nodeValue);
 }
